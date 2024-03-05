32d4af687a3b6986eba6a9a2289c05a49cdc1dd9
hans.arias <hansarias74@gmail.com>
2024-03-04 23:28:26 -0500

    fix unit test to project sort-colors

--- sort-colors/src/test/java/ExerciseColorsTest.java
+++ sort-colors/src/test/java/ExerciseColorsTest.java
@@ -2,47 +2,36 @@ import org.example.ExerciseColors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExerciseColorsTest {
 
     @Test
-    @DisplayName("Should sorted")
+    @DisplayName("Should sorted [0,0,1,1,2,2]")
     void shouldSort1 () {
         int[] numbers = {2,0,2,1,1,0};
         ExerciseColors colors = new ExerciseColors();
         colors.sorted(numbers);
 
-        Arrays.toString(numbers);
-        assertTrue(true);
-        // assertArrayEquals(new int[] {0, 0, 1, 1, 2, 2}, numbers);
+        assertArrayEquals(new int[] {0, 0, 1, 1, 2, 2}, numbers);
     }
 
     @Test
-    @DisplayName("Should sorted 2")
+    @DisplayName("Should sorted [0,1,2]")
     void shouldSort2 () {
         int[] numbers = {2,0,1};
         ExerciseColors colors = new ExerciseColors();
         colors.sorted(numbers);
-        // assertArrayEquals(new int[] {0, 1, 2}, numbers);
-        System.out.println(Arrays.toString(numbers));
-        assertTrue(true);
+        assertArrayEquals(new int[] {0, 1, 2}, numbers);
     }
 
-
     @Test
-    @DisplayName("Should sorted 2")
+    @DisplayName("Should sorted [0,0,0,1,1,1,1,2,2]")
     void shouldSort3 () {
         int[] numbers = {1,0,0,1,1,2,2,0,1};
         ExerciseColors colors = new ExerciseColors();
         colors.sorted(numbers);
-        // assertArrayEquals(new int[] {0, 1, 2}, numbers);
-        System.out.println(Arrays.toString(numbers));
-        assertTrue(true);
+        assertArrayEquals(new int[] {0,0,0,1,1,1,1,2,2}, numbers);
     }
-
-
 
 }
