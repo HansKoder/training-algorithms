5772b240574fb5f3a3ae472c1d871cb9d045f7eb
hans.arias <hansarias74@gmail.com>
2024-03-12 10:38:58 -0500

    solve algorithm passwor-security hackerRank

--- password-security/src/main/java/org/example/PasswordSecurity.java
+++ password-security/src/main/java/org/example/PasswordSecurity.java
@@ -2,33 +2,12 @@ package org.example;
 
 public class PasswordSecurity {
 
-    private final static int ASCII_ZERO = 48;
-    private final static int ASCII_ONE = 49;
-
-    // 1, 0, 0, 1, 1, 0
     public int getNumberOfChanges (String pwd) {
+        int changes = 0;
 
-        int counterZero = 0;
-        int counterOne = 0;
+        for (int i = 0; i < pwd.length() - 1; i++) if (pwd.charAt(i) != pwd.charAt(i + 1)) changes++;
 
-        // loop -> count changes each condition
-        for (int i = 0; i < pwd.length(); i++) {
-            // condition zero
-            char character = pwd.charAt(i);
-            // sequential to 1
-            if (character == ASCII_ZERO) {
-                counterOne++;
-            } else if (character == ASCII_ONE) { // Sequential to 0
-                counterZero++;
-            }
-        }
-
-        // get min
-        int result = Math.min(counterZero, counterOne);
-
-        // get cant of substring length each part
-
-        return result;
+        return Math.min(changes, pwd.length() - changes);
     }
 
 }
